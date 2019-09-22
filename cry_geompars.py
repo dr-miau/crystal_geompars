5a5aed9f97fed1de19300227111ba639f77860c5
dr-miau <marcos.almazo19@gmail.com>
2019-09-22 09:46:51 -0500

    First working version

--- cry_geompars.py
+++ cry_geompars.py
@@ -4,25 +4,24 @@
 #Some details:  -The pattern must be typed between double quotes 
 #                if using wildcards: "pattern*"
 
-import sys,glob,re
-#import glob
-
-#final_geom = re.compile(r"FINAL GEOM*")
+import sys,glob
 
 if len(sys.argv) < 2:
-    print("Specify a pattern for files to parse.")
+    print("Specify a filename or filenames pattern to parse.")
     sys.exit()
 
 filenames = sorted(glob.glob(sys.argv[1]))
-#final_lines=False
+outfile = open("geom_params.dat","w+")
+outfile.write("A\tB\tC\talpha\tbeta\tgamma\tvol\tdens\n")
+truefiles = []
 
 for rfile in filenames:
-    print(rfile)
     final_lines = False
     with open(rfile,'r') as readfile:
         for line in readfile:
             if line.startswith(" FINAL OPT"):
                 final_lines = True
+                truefiles.append(rfile)
             if final_lines and line.startswith(" PRIMITIVE"):
                 temp_line = line.split()
                 volume = temp_line[temp_line.index("VOLUME=")+1]
@@ -30,14 +29,10 @@ for rfile in filenames:
             if final_lines and line.startswith("         A"):
                 cell_params = next(readfile).split()
         if final_lines:
-            print("VOL =",volume)
-            print("DENS =",density)
-            print("A =",cell_params[0])
-            print("B =",cell_params[1])
-            print("C =",cell_params[2])
-            print("alpha =",cell_params[3])
-            print("beta =",cell_params[4])
-            print("gamma =",cell_params[5])
-                
-
-
+            outfile.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"\
+                    .format(cell_params[0],cell_params[1],cell_params[2],
+                    cell_params[3],cell_params[4],cell_params[5],
+                    volume,density))
+    
+for tfile in truefiles:
+    outfile.write("#{}\n".format(tfile))
